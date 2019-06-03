2986c8a06f409ed65b4f734dddbabf856c440d2a
AMHOL <44193988+ShychraNecile@users.noreply.github.com>
2019-06-03 16:20:00 +0200

    Add files via upload

--- HTML/main1.js
+++ HTML/main1.js
@@ -2,22 +2,62 @@
 //spelerAanDeBeurt =  
 var spelerAanDeBeurt;
 var symbool;
+var meteoserverApiKey = '02462ed844';
+var meteoserverUrl = 'https://meteoserver.nl/api/liveweer.php?lat=52.1052957&long=5.1806729&key=';
 
+function showValue(id, value){
+		document.getElementById (id).textContent = value;
+}
+function dateTime(){
+	var currentdate = new Date();
+	var minutes = currentdate.getMinutes();
+	var seconds = currentdate.getSeconds();
+	if (minutes < 10){
+		minutes = "0" + minutes;
+	}
+	if (seconds < 10){
+		seconds = "0" + seconds;
+	}
+	var datetime =  
+		currentdate.getHours() + ":" 
+		+ minutes + ":" + seconds;
+	return datetime;
+}
+function weerUpdate(){
+	$.get(meteoserverUrl + meteoserverApiKey, function(data){
+		var bericht = data.liveweer[0];
+
+		showValue("alarmbijgewerkt", bericht.alarmbijgewerkt);
+		showValue("temperatuur", bericht.temp);
+		showValue("maxTemperatuur", bericht.d0tmax);
+		showValue("minTemperatuur", bericht.d0tmin);
+		showValue("gemTemperatuur", bericht.gtemp);
+		showValue("windrichting", bericht.d0windr);
+		showValue("windkracht", bericht.d0windk);
+		showValue("neerslag", bericht.d0neerslag);
+		showValue("updated", dateTime());
+	})
+}
+function pollweer(){
+	setInterval(function(){
+		weerUpdate();
+	},100000);
+}
 function onload(){
 	var container = document.getElementById("buttonContainer");
 	container.style.display = "none";
 	container = document.getElementById("statusContainer");
 	container.style.display = "none";
-
+	weerUpdate();
+	//pollweer();
 }
-
 
 function startGame(){
 	var p1 = document.getElementById ("player1").value;
 	spelerAanDeBeurt = p1;
-	document.getElementById ("huidigeSpeler").textContent=spelerAanDeBeurt;
+	document.getElementById ("huidigeSpeler").textContent = spelerAanDeBeurt;
 	symbool = "X";
-	document.getElementById ("symbool").textContent=symbool;
+	document.getElementById ("symbool").textContent = symbool;
 	var container = document.getElementById("buttonContainer");
 	container.style.display = "block";
 	container = document.getElementById("statusContainer");
